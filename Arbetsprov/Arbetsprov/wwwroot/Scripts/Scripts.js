f9800fc2ebbf08e8eb2d3dcc9d1a455463887817
Christofer Ryås <plupp86@gmail.com>
2018-05-02 10:55:45 +0200

    Searchfunction gets all characters named Jon

--- Arbetsprov/Arbetsprov/wwwroot/Scripts/Scripts.js
+++ Arbetsprov/Arbetsprov/wwwroot/Scripts/Scripts.js
@@ -1,1 +1,33 @@
-﻿
+﻿//function searchForCharacter(charName) {
+
+//	var divToEdit = document.getElementById("divTag");
+
+//	$.ajax({
+//		url: "/Home/InfoJson",
+//		type: "GET",
+//		data: { "id": movieId },
+//		success: function (result) {
+//			$("#divTag").html(result);
+//		}
+//	});
+//}
+
+function getListOfCharacters() {
+	for (var i = 1; i < 50; i++) {
+		$.ajax({
+			url: "https://anapioficeandfire.com/api/characters?page=" + i + "&pagesize=50",
+			type: "GET",
+			success: function (result) {
+				console.log(result);
+				result.forEach(function (character) {
+					if (character.name.includes('Jon')) {
+						$('#listOfCharacters').append(
+							$('<li>')
+								.append($('<a>').attr('href', character.url)
+									.text(character.name)));
+					}
+				});
+			}
+		});
+	}
+}
