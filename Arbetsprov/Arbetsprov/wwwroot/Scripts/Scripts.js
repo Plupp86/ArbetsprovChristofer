d7be5f0f027044fc530eb9d53d3cd6e4f363288a
Christofer Ryås <plupp86@gmail.com>
2018-05-02 12:32:21 +0200

    Added partial infopage for character information

--- Arbetsprov/Arbetsprov/wwwroot/Scripts/Scripts.js
+++ Arbetsprov/Arbetsprov/wwwroot/Scripts/Scripts.js
@@ -2,20 +2,20 @@
 function getListOfCharacters() {
 	var searchString = $('#searchString').val();
 	$('#listOfCharacters').empty();
+	$('#center').show();
 	for (var i = 1; i < 50; i++) {
 		$.ajax({
 			url: "https://anapioficeandfire.com/api/characters?page=" + i + "&pagesize=50",
 			type: "GET",
 			success: function (result) {
-				console.log(result);
 				result.forEach(function (character) {
 					if (character.name.toLowerCase().includes(searchString.toLowerCase())) {
 						var id = character.url.split('/')[5];
-						console.log(id);
 						$('#listOfCharacters').append(
 							$('<li>')
 								.text(character.name)
-								.attr('onclick', 'infoPage(' + id + ')'));
+								.attr('onclick', 'infoPage(' + id + ')')
+								.addClass('searchResult'));
 					}
 				});
 			}
@@ -29,11 +29,29 @@ function loadCharacterInfo(characterId) {
 		type: "GET",
 		success: function (result) {
 			$('#name').html(result.name);
+			if (result.aliases[0] !== "") {
+				result.aliases.forEach(function (alias) {
+					$('#alias').append(alias + ', ');
+				});
+			}
+
+			$('#born').html(result.born);
+			if (result.died !== "") {
+				$('#died').html(result.died);
+			}
+
+			if (result.playedBy[0] !== "") {
+				$('#actor').html(result.playedBy[0]);
+			}
+
+			$('#url').html('<a href="' + result.url + '">' + result.url + '</a>');
+
 		}
 	});
 }
 
 function infoPage(characterId) {
+	$('#right').show();
 	$.ajax({
 		url: "/Home/Info",
 		type: "GET",
